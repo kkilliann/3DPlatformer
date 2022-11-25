95d74aca988c584f4f2a617e1bf5562112059302
killian <113103283+kkilliann@users.noreply.github.com>
2022-11-25 15:29:44 +0000

    Dear lort

--- Assets/CharacterControl.cs
+++ Assets/CharacterControl.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+
+        }
         
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
